406d0a73d7ada76d74074267e26e41bf06807fc4
khaled <lga.sig.khaled@gmail.com>
2022-11-08 17:27:43 +0100

    localisation

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+    final Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            getLocation();
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
